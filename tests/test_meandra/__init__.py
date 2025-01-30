ddcf68bd5b2fd011d585974ed6d95a7244aeb44f
Esther Poniatowski <esther.poniatowski@ens.psl.eu>
2025-01-30 12:04:24 +0100

    test(*): Initialize the test directory

---
+++ tests/test_meandra/__init__.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+test_meandra
+============
+
+Tests for the meandra package.
+
+Modules
+-------
+test_core
+
+See Also
+--------
+meandra
+    Tested package.
+
+Notes
+-----
+This test package is imported by Sphinx to extract docstrings for documenting tests. This
+documentation provides additional examples and explanations for the tested modules and functions.
+"""
---
+++ tests/test_meandra/test_core/__init__.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+test_meandra.test_core
+======================
+
+Tests for the meandra package, core module.
+
+Modules
+-------
+test_parameters
+
+See Also
+--------
+meandra.core
+    Tested module.
+"""
