7e60bdc1cce389fb5e4fa852e03574f4cdee4157
Esther Poniatowski <esther.poniatowski@ens.psl.eu>
2025-06-08 15:08:31 +0200

    docs(__main__): Improve docstrings

--- src/meandra/__main__.py
+++ src/meandra/__main__.py
@@ -1,16 +1,16 @@
 """
-Command-line entry point for the `meandra` package.
+Entry point for the `meandra` package, invoked as a module.
 
 Usage
 -----
-To invoke the package::
+To launch the command-line interface, execute::
 
     python -m meandra
 
 
 See Also
 --------
-meandra.cli: Command-line interface module for the package.
+meandra.cli: Module implementing the application's command-line interface.
 """
 from .cli import app
 
